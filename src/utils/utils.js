c3f35b5704202e28c2bae3282f00510f9df9407e
WalidOmran <walidomran72@gmail.com>
2026-01-14 14:49:41 +0200

    fix: refactor property page to use new data fetching method and update API URLs for production

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,9 +2,7 @@ export const propertiesApiUrl = process.env.NODE_ENV === 'production'
   ? '/api/proxy/properties'
   : 'http://localhost:9000/properties';
 
-export const apiUrl = process.env.NODE_ENV === 'production'
-  ? '/api/proxy'
-  : 'http://localhost:9000';
+export const apiUrl = '/api/proxy';
 
 
 export const initialFavoritesState = {
