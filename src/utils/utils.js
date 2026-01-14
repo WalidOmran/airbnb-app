7bd8cb7a89453f956c83cc8a21485ca3d35ba1d2
WalidOmran <walidomran72@gmail.com>
2026-01-14 16:34:31 +0200

    fix: enhance data fetching logic with internal proxy handling and dynamic imports

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,9 +1,10 @@
-export const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:9000';
-
+//export const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:9000';
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "https://airbnb-json-db.vercel.app" || 'http://localhost:9000';
+export const apiUrl = process.env.NEXT_PUBLIC_API_URL || "https://airbnb-json-db.vercel.app" || 'http://localhost:9000';
 
 
 export const propertiesApiUrl = `${BASE_URL}/properties`;
-export const apiUrl = BASE_URL;
+//export const apiUrl = BASE_URL;
 
 
 export const initialFavoritesState = {
